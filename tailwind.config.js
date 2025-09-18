bdae4b4c1499bb68a35eb9cb76eef9af4d41c90f
Rahad1230 <rahad2415@gmail.com>
2025-09-18 14:18:36 +0600

    Assainment 5 done

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/*
+ * File: tailwind.config.js
+ * Created: Wednesday, 17th September 2025 2:44:15 pm
+ * Author: Md.Rahad (mdrahad1296@gmail.com)
+ * -----
+ * Last Modified: Wednesday, 17th September 2025 2:44:25 pm
+ * Modified By: Md.Rahad (mdrahad1296@gmail.com)
+ * -----
+ * Copyright (c) 2025 Islamic University
+ */
